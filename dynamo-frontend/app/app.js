935f81ff106f29e9525dce46a5d527a44b1241a3
Guillaume Serre <guillaume.serre@gmail.com>
2016-07-10 20:35:16 +0200

    Angular progress

--- dynamo-frontend/app/app.js
+++ dynamo-frontend/app/app.js
@@ -29,6 +29,18 @@ angular.module('dynamo', [
   $scope.moviesWantedCount = 0;
   $scope.moviesSuggestionCount = 0;
 
+  $scope.magazinesCollectionCount = 0;
+  $scope.magazinesWantedCount = 0;
+  $scope.magazinesSuggestionCount = 0;
+
+  $scope.booksCollectionCount = 0;
+  $scope.booksWantedCount = 0;
+  $scope.booksSuggestionCount = 0;
+
+  $scope.musicAlbumsCollectionCount = 0;
+  $scope.musicAlbumsWantedCount = 0;
+  $scope.musicAlbumsSuggestionCount = 0;
+
   downloadableService.counts().then( function(response) {
 
     var counts = response.data;
@@ -41,8 +53,35 @@ angular.module('dynamo', [
         } else if (counts[i].status == 'SUGGESTED') {
           $scope.moviesSuggestionCount = counts[i].count;
         }
-
       }
+      if (counts[i].type == 'MagazineIssue') {
+        if (counts[i].status == 'DOWNLOADED') {
+          $scope.magazinesCollectionCount = counts[i].count;
+        } else if (counts[i].status == 'DOWNLOADED' || counts[i].status == 'DOWNLOADED') {
+          $scope.magazinesWantedCount += counts[i].count;
+        } else if (counts[i].status == 'SUGGESTED') {
+          $scope.magazinesSuggestionCount = counts[i].count;
+        }
+      }
+      if (counts[i].type == 'Book') {
+        if (counts[i].status == 'DOWNLOADED') {
+          $scope.booksCollectionCount = counts[i].count;
+        } else if (counts[i].status == 'DOWNLOADED' || counts[i].status == 'DOWNLOADED') {
+          $scope.booksWantedCount += counts[i].count;
+        } else if (counts[i].status == 'SUGGESTED') {
+          $scope.booksSuggestionCount = counts[i].count;
+        }
+      }
+      if (counts[i].type == 'MusicAlbum') {
+        if (counts[i].status == 'DOWNLOADED') {
+          $scope.musicAlbumsCollectionCount = counts[i].count;
+        } else if (counts[i].status == 'DOWNLOADED' || counts[i].status == 'DOWNLOADED') {
+          $scope.musicAlbumsWantedCount += counts[i].count;
+        } else if (counts[i].status == 'SUGGESTED') {
+          $scope.musicAlbumsSuggestionCount = counts[i].count;
+        }
+      }
+      
     }
 
     // TODO
