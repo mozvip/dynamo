42383b62249d135eb76ee0dd4da787ecae682af0
Guillaume Serre <guillaume.serre@gmail.com>
2016-09-17 19:11:14 +0200

    Cleanup install script

--- dynamo-ops/install-service.sh
+++ dynamo-ops/install-service.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 SERVICE_NAME=dynamo
-SERVICE_USER=dynamo
-SERVICE_GROUP=dynamo
+
+cp etc/default/$SERVICE_NAME /etc/default/$SERVICE_NAME
+[ -r /etc/default/$SERVICE_NAME ] && . /etc/default/$SERVICE_NAME
 
 cp etc/init.d/$SERVICE_NAME /etc/init.d/$SERVICE_NAME
-cp etc/default/$SERVICE_NAME /etc/default/$SERVICE_NAME
 chmod +x /etc/init.d/$SERVICE_NAME
 
 mkdir /var/log/$SERVICE_NAME
 chown $SERVICE_USER:$SERVICE_GROUP /var/log/$SERVICE_NAME
-mkdir /var/lib/$SERVICE_NAME
-chown $SERVICE_USER:$SERVICE_GROUP /var/lib/$SERVICE_NAME
+mkdir $SERVICE_HOME
+chown $SERVICE_USER:$SERVICE_GROUP $SERVICE_HOME
 mkdir /var/run/$SERVICE_NAME
 chown $SERVICE_USER:$SERVICE_GROUP /var/run/$SERVICE_NAME
 update-rc.d $SERVICE_NAME defaults
