874ceadf1276d576580f3e494fd2f14caadc679a
Guillaume Serre <guillaume.serre@gmail.com>
2016-09-17 19:07:40 +0200

    Fix service install script

--- dynamo-ops/install-service.sh
+++ dynamo-ops/install-service.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
 SERVICE_NAME=dynamo
+SERVICE_USER=dynamo
+SERVICE_GROUP=dynamo
+
+cp etc/init.d/$SERVICE_NAME /etc/init.d/$SERVICE_NAME
+cp etc/default/$SERVICE_NAME /etc/default/$SERVICE_NAME
+chmod +x /etc/init.d/$SERVICE_NAME
 
 mkdir /var/log/$SERVICE_NAME
 chown $SERVICE_USER:$SERVICE_GROUP /var/log/$SERVICE_NAME
