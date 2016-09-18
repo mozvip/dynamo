254f4ceb7570134d046e5109ff6713a1299ff0d7
Guillaume Serre <guillaume.serre@gmail.com>
2016-09-18 18:25:30 +0200

    Some work on Trakt support

--- dynamo-frontend/app/configuration/trakt.js
+++ dynamo-frontend/app/configuration/trakt.js
@@ -14,18 +14,22 @@ angular.module('dynamo.trakt', ['ngRoute', 'ngResource'])
   });
 }])
 
-.factory('traktService', ['BackendService', function(BackendService){
-  var traktService = {};
-
-  return traktService;
-}])
-
-.controller('TraktCtrl', ['$scope', 'traktService', 'configuration', 'BackendService', function( $scope, traktService, configuration, BackendService ) {
+.controller('TraktCtrl', ['$scope', 'configuration', 'configurationService', 'BackendService', function( $scope, configuration, configurationService, BackendService ) {
 
   $scope.authorizeURL = 'https://trakt.tv/oauth/authorize?client_id=1f93ab28686a87d36c0e198f15a34ba7c0d3fb45cbd3303515da246718b570a6&redirect_uri=urn%3Aietf%3Awg%3Aoauth%3A2.0%3Aoob&response_type=code';
 
-  $scope.authorize = function() {
-    
+  $scope.config = configuration.data;
+
+  $scope.itemsToConfigure = [
+      $scope.config['TraktManager.enabled'],
+      $scope.config['TraktManager.username']
+  ];
+
+  $scope.pinCode = '';
+
+  $scope.saveSettings = function () {
+    BackendService.post('trakt/auth/' + $scope.pinCode);
+    configurationService.saveItems($scope.itemsToConfigure);
   }
 
 }]);
