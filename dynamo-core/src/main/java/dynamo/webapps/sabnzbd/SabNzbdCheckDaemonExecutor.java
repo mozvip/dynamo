69aebf5df53e35c5004b17cd7660cb67e18f8c72
Guillaume Serre <guillaume.serre@gmail.com>
2016-09-25 19:50:59 +0200

    Cleanup detection of failed downloads with SABNZBD

--- dynamo-core/src/main/java/dynamo/webapps/sabnzbd/SabNzbdCheckDaemonExecutor.java
+++ dynamo-core/src/main/java/dynamo/webapps/sabnzbd/SabNzbdCheckDaemonExecutor.java
@@ -6,6 +6,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Optional;
 
 import org.apache.commons.lang3.StringUtils;
 
@@ -33,89 +34,81 @@ public class SabNzbdCheckDaemonExecutor extends TaskExecutor<SabNzbdCheckDaemonT
 	@Override
 	public void execute() throws Exception {
 
-		SabNzbdResponse queue = sab.getQueue();
-		if (queue.getQueue() != null) {
-			for (SabNzbdResponseSlot slot : queue.getQueue().getSlots()) {
-				if ("Paused".equals (slot.getStatus() ) && slot.getFilename().startsWith("ENCRYPTED / ")) {
-					sab.delete( slot.getNzo_id() );
-					SearchResult result = searchResultDAO.findSearchResultByClientId( slot.getNzo_id() );
-					if (result != null) {
-						DownloadableManager.getInstance().redownload(result.getDownloadableId());
-					}
-				}
-			}
-		}
+		SABHistoryResponse response = sab.getHistory();
 
 		List<SearchResult> results = searchResultDAO.getActiveSearchResults( SearchResultType.NZB );
-		SABHistoryResponse response = sab.getHistory();
-		for (SabNzbdResponseSlot slot : response.getSlots()) {
+		for (SearchResult searchResult : results) {
 			
-			boolean failed = StringUtils.equals(slot.getStatus(), "Failed");
-			boolean completed = StringUtils.equals(slot.getStatus(), "Completed"); 
-			
-			if (!(failed || completed)) {
-				continue;
-			}
-			
-			if (failed) {
-				sab.deleteFromHistory( slot.getNzo_id() );
-			}
-			
-			for (SearchResult searchResult : results) {
-				if (searchResult.getClientId() != null && searchResult.getClientId().equals( slot.getNzo_id())) {
+			Downloadable downloadable = DownloadableFactory.getInstance().createInstance( searchResult.getDownloadableId() );
 
-					Downloadable downloadable = DownloadableFactory.getInstance().createInstance( searchResult.getDownloadableId() );
-					if (downloadable == null) {
-						searchResultDAO.deleteResultForDownloadableId( searchResult.getDownloadableId() );
-						// TODO: remove files 
-					}
-					if (!downloadable.isDownloaded()) {
+			Optional<SabNzbdResponseSlot> optSlot = response.getSlots().stream().filter( slot -> slot.getNzo_id().equals(searchResult.getClientId())).findFirst();
+
+			if (optSlot.isPresent()) {
+				
+				SabNzbdResponseSlot slot = optSlot.get();
+				
+				boolean failed = StringUtils.equals(slot.getStatus(), "Failed");
+				boolean completed = StringUtils.equals(slot.getStatus(), "Completed"); 
+				
+				if (!(failed || completed)) {
+					continue;
+				}
+				
+				if (failed) {
+					sab.deleteFromHistory( slot.getNzo_id() );
+				}
+				
+				if (downloadable == null) {
+					searchResultDAO.deleteResultForDownloadableId( searchResult.getDownloadableId() );
+					// TODO: remove files 
+				}
+				if (!downloadable.isDownloaded()) {
+					
+					if (failed) {
+						DownloadableManager.getInstance().redownload( downloadable );
+					} else {
+					
+						Path sourceFolder = Paths.get( slot.getStorage() ).toAbsolutePath();
 						
-						if (failed) {
-							DownloadableManager.getInstance().redownload( downloadable );
+						List<Path> files;
+						if (!Files.isDirectory( sourceFolder )) {
+							files = new ArrayList<>();
+							files.add( sourceFolder );
+							sourceFolder = sourceFolder.getParent();
 						} else {
-						
-							Path sourceFolder = Paths.get( slot.getStorage() ).toAbsolutePath();
-							
-							List<Path> files;
-							if (!Files.isDirectory( sourceFolder )) {
-								files = new ArrayList<>();
-								files.add( sourceFolder );
-								sourceFolder = sourceFolder.getParent();
-							} else {
-								files = FolderManager.getAllFilesFrom( sourceFolder, true );
-							}
-	
-							// FIXME: sourceFolder is local to the SABnzbd server, not to the dynamo server !!!
-							// This only works if SABnzbd and dynamo are on the same server !
-	
-							for (Iterator<Path> iterator = files.iterator(); iterator.hasNext();) {
-								Path path = iterator.next();
-								if (!Files.exists(path)) {
-									iterator.remove();
-								} else if (DownloadableManager.getInstance().isBlackListed(path, downloadable)) {
-									iterator.remove();
-									queue( new DeleteTask(path, false), false );
-								}
-							}
-	
-							if (files != null && files.size() > 0) {
-								try {
-									DownloadableManager.getInstance().downloaded(task, downloadable, searchResult, sourceFolder, files, true );
-								} catch (Exception e) {
-									ErrorManager.getInstance().reportThrowable(task, e);
-								}
-							} else {
-								sab.deleteFromHistory( slot.getNzo_id() );
-								DownloadableManager.getInstance().redownload( downloadable );
+							files = FolderManager.getAllFilesFrom( sourceFolder, true );
+						}
+
+						// FIXME: sourceFolder is local to the SABnzbd server, not to the dynamo server !!!
+						// This only works if SABnzbd and dynamo are on the same server !
+
+						for (Iterator<Path> iterator = files.iterator(); iterator.hasNext();) {
+							Path path = iterator.next();
+							if (!Files.exists(path)) {
+								iterator.remove();
+							} else if (DownloadableManager.getInstance().isBlackListed(path, downloadable)) {
+								iterator.remove();
+								queue( new DeleteTask(path, false), false );
 							}
 						}
+
+						if (files != null && files.size() > 0) {
+							try {
+								DownloadableManager.getInstance().downloaded(task, downloadable, searchResult, sourceFolder, files, true );
+							} catch (Exception e) {
+								ErrorManager.getInstance().reportThrowable(task, e);
+							}
+						} else {
+							sab.deleteFromHistory( slot.getNzo_id() );
+							DownloadableManager.getInstance().redownload( downloadable );
+						}
 					}
-					
-					break;
 				}
+				
+			} else {
+				// download is not present from queue, redownload
+				DownloadableManager.getInstance().redownload( downloadable );
 			}
-			
 		}
 	}
 
