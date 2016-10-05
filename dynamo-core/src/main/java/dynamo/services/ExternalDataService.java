8c1a6b0aa3d570bf0fb0912ee3f800a30e610fa6
Guillaume Serre <guillaume.serre@cgi.com>
2016-10-05 08:42:35 +0200

    Missing image

--- dynamo-core/src/main/java/dynamo/services/ExternalDataService.java
+++ dynamo-core/src/main/java/dynamo/services/ExternalDataService.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.StandardOpenOption;
+import java.util.Calendar;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -21,21 +22,28 @@ public class ExternalDataService {
 		java.nio.file.Path path = LocalImageCache.getInstance().resolveLocal( url );
 		if (!Files.isReadable( path )) {
 			
-			// FIXME : search for missing image in this case
+			if (url.endsWith(".jpg")) {
+				// TODO : search for missing image in this case
+				
+				InputStream in = this.getClass().getResourceAsStream("/ring.svg");
+				return Response.ok(in)
+						.header("Content-Type", "image/svg+xml")
+						.build();				
+			}
 			
 			return Response.status(404).build();
 		} else {
 			
+			Calendar calendar = Calendar.getInstance();
+			calendar.add( Calendar.DAY_OF_MONTH, 2);
+
 			InputStream in = Files.newInputStream(path, StandardOpenOption.READ);
 			return Response.ok(in)
 					.header("Content-Length", Files.size(path))
 					.header("Content-Type", Files.probeContentType(path))
+					.header("Expires", calendar.getTimeInMillis())
 					.build();			
-			
-//			Calendar calendar = Calendar.getInstance();
-//			calendar.add( Calendar.DAY_OF_MONTH, 7);
-//			
-//			response.addDateHeader("Expires", calendar.getTimeInMillis());
+
 		}		
 	}
 
