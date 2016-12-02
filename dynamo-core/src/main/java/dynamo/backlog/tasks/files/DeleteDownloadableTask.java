e513a9f97430c93cebb4dafa8fdc2dff3898fa6e
Guillaume Serre <guillaume.serre@gmail.com>
2016-12-02 21:45:48 +0100

    DeleteDownloadableTask is now Immediate

--- dynamo-core/src/main/java/dynamo/backlog/tasks/files/DeleteDownloadableTask.java
+++ dynamo-core/src/main/java/dynamo/backlog/tasks/files/DeleteDownloadableTask.java
@@ -2,12 +2,13 @@ package dynamo.backlog.tasks.files;
 
 import java.lang.reflect.InvocationTargetException;
 
+import dynamo.backlog.tasks.core.ImmediateTask;
 import dynamo.core.LogQueuing;
 import dynamo.core.manager.DownloadableFactory;
 import dynamo.core.model.DownloadableTask;
 import dynamo.model.Downloadable;
 
-public class DeleteDownloadableTask extends DownloadableTask implements LogQueuing {
+public class DeleteDownloadableTask extends DownloadableTask implements ImmediateTask, LogQueuing {
 	
 	public DeleteDownloadableTask( long id ) throws ClassNotFoundException, IllegalAccessException, InvocationTargetException {
 		this( DownloadableFactory.getInstance().createInstance(id) );
