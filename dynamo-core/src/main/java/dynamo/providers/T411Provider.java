28f5279103cee5edc3007d7922a986bdd984dff7
Guillaume Serre <guillaume.serre@gmail.com>
2017-01-05 14:11:33 +0100

    Fix episode number for T411 search

--- dynamo-core/src/main/java/dynamo/providers/T411Provider.java
+++ dynamo-core/src/main/java/dynamo/providers/T411Provider.java
@@ -174,7 +174,7 @@ public class T411Provider extends DownloadFinder implements BookFinder, EpisodeF
 		String searchParams = String.format("%s S%02dE%02d", searchString, seasonNumber, episodeNumber);
 		
 		String additionalParams = "term[45][]=" + ( 967 + seasonNumber );
-		additionalParams += "&term[46][]=" + ( 936 + episodeNumber );
+		additionalParams += "&term[46][]=" + ( 936 + (episodeNumber+1) );
 
 		List<SearchResult> results = new ArrayList<SearchResult>();
 		results.addAll( searchVideo( searchParams, audioLanguage, 433, additionalParams ) );
