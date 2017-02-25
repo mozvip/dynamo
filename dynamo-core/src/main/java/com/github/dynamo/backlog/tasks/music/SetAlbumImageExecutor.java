1ea9fec0b9adc59186cd47aab1e1f3a54a433b3a
Guillaume Serre <guillaume.serre@gmail.com>
2017-02-25 12:51:02 +0100

    Remove dead code related to music management

--- dynamo-core/src/main/java/com/github/dynamo/backlog/tasks/music/SetAlbumImageExecutor.java
+++ dynamo-core/src/main/java/com/github/dynamo/backlog/tasks/music/SetAlbumImageExecutor.java
@@ -3,11 +3,8 @@ package com.github.dynamo.backlog.tasks.music;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
-import java.util.List;
 
-import com.github.dynamo.backlog.BackLogProcessor;
 import com.github.dynamo.core.model.TaskExecutor;
-import com.github.dynamo.model.music.MusicFile;
 import com.github.dynamo.music.jdbi.MusicAlbumDAO;
 
 public class SetAlbumImageExecutor extends TaskExecutor<SetAlbumImageTask> {
@@ -23,13 +20,6 @@ public class SetAlbumImageExecutor extends TaskExecutor<SetAlbumImageTask> {
 	public void execute() throws Exception {
 		
 		long albumId = task.getMusicAlbum().getId();
-		
-		List<MusicFile> files = musicDAO.findMusicFiles( albumId );
-		if (files != null && files.size() > 0) {
-			for (MusicFile file : files) {
-				BackLogProcessor.getInstance().schedule( new SynchronizeMusicTagsTask( file.getFilePath() ), false );	// update file tag
-			}
-		}
 
 		Path albumPath = task.getMusicAlbum().getFolder();
 		if (albumPath != null) {
