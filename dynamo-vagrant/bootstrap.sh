5af9d2e4e48a3444da9766c4d507564186c9e88a
Guillaume Serre <guillaume.serre@gmail.com>
2017-03-17 16:14:02 +0100

    Vagrant work

--- dynamo-vagrant/bootstrap.sh
+++ dynamo-vagrant/bootstrap.sh
@@ -3,6 +3,7 @@
 apt-get update
 apt-get install -y openjdk-8-jdk
 apt-get install -y nginx
+apt-get install -y mediainfo
 apt-get install -y transmission-daemon
 apt-get install -y sabnzbdplus
 apt-get install -y python-pip
@@ -15,5 +16,11 @@ cp /vagrant/etc/transmission-daemon/settings.json /etc/transmission-daemon/setti
 service transmission-daemon start
 
 adduser --system sabnzbd --disabled-login --disabled-password
+adduser --system dynamo --disabled-login --disabled-password
+
 cp /vagrant/etc/default/sabnzbdplus /etc/default/sabnzbdplus
-service transmission-daemon restart
+service sabnzbdplus start
+
+cd /dynamo/dynamo
+wget -q https://github.com/glowroot/glowroot/releases/download/v0.9.12/glowroot-0.9.12-dist.zip
+unzip glowroot-0.9.12-dist.zip
